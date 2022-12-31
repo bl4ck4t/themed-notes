783bd2842596f972ef96c81c09ad96e919d716cb
Vinay Poojary <vp8448@gmail.com>
2022-12-31 20:47:53 +0530

    Implementing Notes feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { NavLink, Link} from 'react-router-dom'
 import { ThemeContext } from "../contexts/ThemeContext"
 import styles from "../css/navbar.module.css"
 
@@ -10,13 +11,18 @@ export default function Navbar() {
         <div className={styles.navbar}>
             <div className={styles["left-bar"]}>
                 <span className={styles.logo}>
-                    Toggle Theme
+                    <Link className={styles.home} to="/">Toggle Theme</Link>
                 </span>
             </div>
-            
+
+            <div className={styles["menu-items"]}>
+                <span className={styles["menu-item"]}><NavLink to="/new"> New Note</NavLink></span>
+                <span className={styles["menu-item"]}><NavLink to="/edit">Edit</NavLink></span>
+            </div>
+
             <div className={styles["right-bar"]}>     
                 <button onClick={toggleTheme} id={styles["toggle-theme"]} className={`material-symbols-outlined ${styles[themeName == "dark" ? "dark" : "light"]}`}>
-                {themeName == "light" ? "nights_stay" : "sunny"}
+                {themeName == "light" ? "dark_mode" : "sunny"}
                 </button>
             </div>
         </div>
--- src/contexts/themes.ts
+++ src/contexts/themes.ts
@@ -2,11 +2,15 @@ import { CSSProperties } from "react";
 
 export const themes = {
     light: {
-        main: '#fff' as CSSProperties,
-        text: '#000' as CSSProperties
+        main: '#f3f5f4' as CSSProperties,
+        text: '#000' as CSSProperties,
+        containerBackground: '#f3f5f7' as CSSProperties,
+        boxShadow : '2px 2px 50px gray'
     },
     dark: {
-        main: '#000' as CSSProperties,
-        text: '#fff' as CSSProperties
+        main: '#35363a' as CSSProperties,
+        text: '#fff' as CSSProperties,
+        containerBackground: '#333743' as CSSProperties,
+        boxShadow : '2px 2px 50px black'
     }
 };
