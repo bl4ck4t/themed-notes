655354e2e96d30612ead89342392da6890b14f0d
Vinay Poojary <vp8448@gmail.com>
2022-12-31 20:47:53 +0530

    Implementing Notes feature

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -2,7 +2,7 @@ import { createContext, CSSProperties, useEffect, useState } from "react"
 import { themes } from "./themes";
 
 type ThemeContextProviderProps = {
-    children : React.ReactNode
+    children: React.ReactNode
 }
 
 type ThemeName = "light" | "dark";
@@ -14,32 +14,34 @@ type ThemeProps = {
 
 export const ThemeContext = createContext({
     themeName: "dark" as ThemeName,
-    toggleTheme: () => {}
+    toggleTheme: () => { }
 });
 
-export default function ThemeContextProvider( {children} : ThemeContextProviderProps ) {
+export default function ThemeContextProvider({ children }: ThemeContextProviderProps) {
+    const preferredTheme : ThemeName = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 
-    const [themeName, setThemeName] = useState<ThemeName>("dark");
+    const [themeName, setThemeName] = useState<ThemeName>(preferredTheme);
     const [theme, setTheme] = useState<ThemeProps>(themes[themeName] as ThemeProps);
 
     const toggleTheme = () => {
         theme === themes.dark ? (setTheme(themes["light"] as ThemeProps), setThemeName("light")) : (setTheme(themes["dark"] as ThemeProps), setThemeName("dark"));
     };
 
-    function setCSSVariables () {
-        let value : keyof ThemeProps;
+    function setCSSVariables() {
+        let value: keyof ThemeProps;
         for (value in theme) {
-          document.documentElement.style.setProperty(`--${value}`, `${theme[value] as CSSProperties}`);
+            document.documentElement.style.setProperty(`--${value}`, `${theme[value] as CSSProperties}`);
         }
-      };
+    };
 
     useEffect(() => {
+        console.log("Use effect called")
         setCSSVariables();
     }, [toggleTheme])
 
-  return (
-    <ThemeContext.Provider value={{themeName, toggleTheme}}>
-      {children}
-    </ThemeContext.Provider>
-  )
+    return (
+        <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
 }
